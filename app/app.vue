1b3eb9746dd730d8effd6c8cdc386039c9627367
SenghongH <senghong.hang@techbodia.com>
2025-11-22 22:16:30 +0700

    add ga

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-G5XX198FZB"></script>
-      <script>
+  <NuxtPage />
+</template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-G5XX198FZB",
+      async: true,
+    },
+    {
+      children: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-G5XX198FZB');
-      </script>
-    </head>
-    <NuxtPage/>
-  </div>
-</template>
+      `
+    }
+  ]
+})
+</script>
