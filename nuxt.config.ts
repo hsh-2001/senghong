0be971bd7de30f3385d25aabfd3e8364863e335a
senghong <senghong.learning@gmail.com>
2026-05-15 15:05:57 +0700

    Fix after upgrade Nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  experimental: {
+    viteEnvironmentApi: true,
+  },
   css: ['./app/assets/main.css'],
   vite: {
     plugins: [
